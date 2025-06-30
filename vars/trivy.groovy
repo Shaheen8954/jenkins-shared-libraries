4e662dc35f9443f83a9bbb54036a05f348cc3747
Shaheen8954 <nshaheen488@gmail.com>
2025-07-01 02:24:48 +0530

    Update trivy.groovy

--- vars/trivy.groovy
+++ vars/trivy.groovy
@@ -2,7 +2,7 @@ def call(String DockerHubUser, String ProjectName, String ImageTag) {
     sh """
     docker run --rm \
         -v /var/run/docker.sock:/var/run/docker.sock \
-        aquasec/trivy:latest image --exit-code 1 --severity HIGH,CRITICAL \
+        aquasec/trivy:latest image --exit-code 0 --severity HIGH,CRITICAL \
         --format table --output trivy-repo.txt ${DockerHubUser}/${ProjectName}:${ImageTag}
         """
 }
