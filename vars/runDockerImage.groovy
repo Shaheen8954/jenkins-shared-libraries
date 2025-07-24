e4f0593ddf5fccd3825b34a697a05e4d9a986abf
Shaheen8954 <nshaheen488@gmail.com>
2025-07-24 11:33:21 +0530

    Update runDockerImage.groovy

--- vars/runDockerImage.groovy
+++ vars/runDockerImage.groovy
@@ -5,5 +5,7 @@
 def call(String DockerHubUser, String ProjectName, String ImageTag) {
   echo "running the container..."
   echo "Running Docker image: ${DockerHubUser}/${ProjectName}:${ImageTag}"
-  sh "docker run -d -p ${PortNumber}/${DockerHubUser}/${ProjectName}:${ImageTag}"
+  sh "docker stop ${ProjectName}
+  sh "docker rm ${ProjectName}
+  sh "docker run -d -p ${PortNumber}/ --name ${ProjectName}/${ProjectName}:${ImageTag}"
 }
