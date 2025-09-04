f4acc01304fafffc970025fd6f8eb2265f62b2b6
Shaheen8954 <nshaheen488@gmail.com>
2025-09-04 20:05:56 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,3 +1,8 @@
 def call(String DockerHubUser, String ProjectName, String ImageTag) {
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DockerHubUser', passwordVariable: 'DOCKER_PASS')]) {
+        sh """
+            echo "$DOCKER_PASS" | docker login -u "$DockerHubUser" --password-stdin
+            docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} .
+        """
+    }
 }
